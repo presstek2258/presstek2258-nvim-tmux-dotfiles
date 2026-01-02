279b17d14e4006a1e2d57f9818213f1d6bde3257
Adrian Ramirez <144853208+presstek2258@users.noreply.github.com>
2026-01-02 13:49:07 -0500

    mason.nvim and mason-laspconfig.nvim are no under mason-org

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -2,13 +2,13 @@ LSPS = { "lua_ls", "clangd", "harper_ls", "pylsp", "jdtls", "ts_ls", "cssls", "h
 
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = LSPS,
